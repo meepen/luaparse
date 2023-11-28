d01ba78abf1bb71ef25b251734419d24d02305e8
meepen <meepdarknessmeep@gmail.com>
2023-11-28 16:17:27 -0500

    remove unused NodeType values

--- src/node.ts
+++ src/node.ts
@@ -2,26 +2,16 @@ import { Node } from './nodes/types.js';
 
 export enum NodeType {
   Chunk = 'Chunk',
-  Block = 'Block',
   Statement = 'Statement',
-  LastStatement = 'LastStatement',
   FunctionName = 'FunctionName',
   VariableList = 'VariableList',
-  Variable = 'Variable',
   NameList = 'NameList',
   ExpressionList = 'ExpressionList',
   Expression = 'Expression',
-  FunctionCall = 'FunctionCall',
   Arguments = 'Arguments',
   Function = 'Function',
-  FunctionBody = 'FunctionBody',
   ParameterList = 'ParameterList',
-  TableConstructor = 'TableConstructor',
-  FieldList = 'FieldList',
   Field = 'Field',
-  FieldSeperator = 'FieldSeperator',
-  BinaryOperation = 'BinaryOperation',
-  UnaryOperation = 'UnaryOperation',
   FuncBody = 'FuncBody',
 
   /* Interpreted from the manual but not explicitly stated */
