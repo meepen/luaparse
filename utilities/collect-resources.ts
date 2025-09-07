877006cf82cbcf4273ceec79ab6bef0c0f42fc11
meepen <meep@meepen.dev>
2025-09-07 12:46:24 -0400

    add test command `npm run test:resource -- <name>`

--- utilities/collect-resources.ts
+++ utilities/collect-resources.ts
@@ -1,19 +1,33 @@
-import { readdir, readFile } from 'node:fs/promises';
-import { join, resolve } from 'node:path';
+import { readdir, readFile, stat } from 'node:fs/promises';
+import { join } from 'node:path';
 
-export async function collectLuaFiles() {
+export async function collectAllResourcesFromPath(path: string): Promise<{ name: string; content: string }[]> {
   // read ./lua directory and return all .lua files as an array of strings
-  const baseDir = resolve(join(import.meta.dirname, '../resources/lua'));
+  const det = await stat(path);
+  if (!det.isDirectory()) {
+    if (!path.endsWith('.lua')) {
+      return [];
+    }
 
-  const files = await readdir(baseDir);
-  return await Promise.all(
-    files
-      .map((file) => join(baseDir, file))
-      .filter((file) => file.endsWith('.lua'))
-      .sort((a, b) => (a < b ? -1 : 1))
-      .map(async (file) => ({
-        name: file.split('/').pop()!,
-        content: await readFile(file, 'utf-8'),
-      })),
-  );
+    return [
+      {
+        name: path,
+        content: await readFile(path, 'utf-8'),
+      },
+    ];
+  }
+
+  const files = await readdir(path);
+  return (
+    await Promise.all(
+      files
+        .map((file) => join(path, file))
+        .sort((a, b) => (a < b ? -1 : 1))
+        .map(collectAllResourcesFromPath),
+    )
+  ).flat();
+}
+
+export async function collectLuaFiles(filePaths: string[] = ['resources/lua']) {
+  return (await Promise.all(filePaths.map(async (path) => collectAllResourcesFromPath(path)))).flat();
 }
--- utilities/test.ts
+++ utilities/test.ts
@@ -1,27 +1,45 @@
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
 import { createParser, LuaVersion } from '../src/index.js';
 import { collectLuaFiles } from './collect-resources.js';
 
-const luaSource = await collectLuaFiles();
+await yargs(hideBin(process.argv))
+  .command(
+    'test [files..]',
+    'Test the parser with Lua files',
+    (y) =>
+      y.positional('files', {
+        describe: 'Lua files to parse',
+        type: 'string',
+        array: true,
+        default: [],
+      }),
+    async (argv) => {
+      console.log('Starting parser tests...');
+      const luaSource = await collectLuaFiles(argv.files.length !== 0 ? argv.files : undefined);
 
-const luaVersion = LuaVersion.PUCRio_v5_1;
+      const luaVersion = LuaVersion.PUCRio_v5_1;
 
-const results: { result?: unknown; error?: Error; file: { name: string; content: string } }[] = [];
-for (const file of luaSource) {
-  console.log(`Parsing ${file.name}...`);
-  try {
-    results.push({
-      result: await createParser(luaVersion, file.content).parse(),
-      file,
-    });
-  } catch (error) {
-    results.push({ error: error instanceof Error ? error : new Error(String(error)), file });
-  }
-}
+      const results: { result?: unknown; error?: Error; file: { name: string; content: string } }[] = [];
+      for (const file of luaSource) {
+        console.log(`Parsing ${file.name}...`);
+        try {
+          results.push({
+            result: await createParser(luaVersion, file.content).parse(),
+            file,
+          });
+        } catch (error) {
+          results.push({ error: error instanceof Error ? error : new Error(String(error)), file });
+        }
+      }
 
-for (const { error, file } of results) {
-  if (error) {
-    console.error(`Error parsing ${file.name}:`, error);
-  } else {
-    console.log(`Successfully parsed ${file.name}`);
-  }
-}
+      for (const { error, file } of results) {
+        if (error) {
+          console.error(`Error parsing ${file.name}:`, error);
+        } else {
+          console.log(`Successfully parsed ${file.name}`);
+        }
+      }
+    },
+  )
+  .parse();
